4b479690a36c2739634805561a448b3aee5958bc
LAMMU <chen_yongchao@126.com>
2023-12-08 17:02:20 +0800

    tsconfig配置支持ts-node运行index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import { Project, SyntaxKind } from "ts-morph";
 
 /**
  * 解析openapi文件，得到object对象
@@ -12,6 +13,31 @@ function parseOpenApiFile(filePath: string) {
 
 function main() {
   const openapiObject = parseOpenApiFile('./swagger.demo.json');
+  const project = new Project();
+  const sourceFile = project.createSourceFile('nestSDK.ts', "");
+  console.log(sourceFile)
+  // const classDeclaration = sourceFile.addClass({
+  //   name: "ExampleClass",
+  // });
+
+  // classDeclaration.addProperty({
+  //   name: "exampleProperty",
+  //   type: "string",
+  //   initializer: `"Hello, World!"`,
+  // });
+  
+  // const exampleMethod = classDeclaration.addMethod({
+  //   name: "exampleMethod",
+  //   returnType: "void",
+  // });
+  // exampleMethod.setBodyText(`console.log(this.exampleProperty);`);
+
+  // sourceFile.formatText({
+  //   indentSize: 2,
+  //   convertTabsToSpaces: true,
+  // });
+
+  // console.log(sourceFile.getFullText());
 }
 
 main();
