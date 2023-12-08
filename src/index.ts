b065fdd1fd3c0246e9af0288bd31385065fedd36
LAMMU <chen_yongchao@126.com>
2023-12-08 16:36:37 +0800

    解析文件json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import * as fs from 'fs';
 
-const content = fs.readFileSync('./package.json', 'utf-8');
-console.log(content);
+/**
+ * 解析openapi文件，得到object对象
+ * @param filePath 文件路径
+ * @returns openapi对象
+ */
+function parseOpenApiFile(filePath: string) {
+  const contentStr = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(contentStr);
+}
+
+function main() {
+  const openapiObject = parseOpenApiFile('./swagger.demo.json');
+}
+
+main();
