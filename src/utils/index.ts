97f92c457f940a041ee941f3d68663769a8266c3
LAMMU <chen_yongchao@126.com>
2023-12-11 17:49:26 +0800

    utils方法

---
+++ src/utils/index.ts
@@ -0,0 +1,15 @@
+import { CodeBlockWriter, SourceFile } from 'ts-morph';
+
+/** 将数组字符串写入到writer */
+export function writeArray(writer: CodeBlockWriter, array: string[], newLine = true) {
+  array.forEach((str, index) => writer.write(str).conditionalNewLine(newLine))
+}
+
+/** 多行注释 */
+export function writeStatements(sourceFile: SourceFile, array: string[]) {
+  sourceFile.addStatements(writer => {
+    writer.write('/**').conditionalNewLine(true);
+    array.forEach(statement => writer.write(' *' + statement).conditionalNewLine(true))
+    writer.write(' */').conditionalNewLine(true);
+  })
+}
