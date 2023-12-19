c42b42a9eb4c58b32402a17c1ffcc0cf0e4072c3
LAMMU <chen_yongchao@126.com>
2023-12-20 00:18:46 +0800

    模版更新

--- sdk.ts
+++ sdk.ts
@@ -2,86 +2,85 @@ import { ApplicationZod, ApplicationDto } from './zod';
 import { ApplicationZodCreate, ApplicationDtoCreate } from './zodExt/Application;
 
 /**
- * 父类初始化参数类型
+ * 基础对象
  */
-interface OptionsType {
-  baseUrl?: string
-  request: typeof request
+interface BaseObj<T = any> {
+  [key: string]: T;
+}
+
+/**
+ * 请求类初始化参数类型
+ */
+interface RequestInitType {
+  /** 请求url中的baseUrl */
+  origin?: string
+  /** 自定义请求函数, 默认使用fetch */
+  request: typeof _fetch_
+}
+
+/**
+ * 请求参数
+ */
+interface RequestType {
+  /** 请求方法 */
+  method: 'get' | 'post' | 'delete' | 'put'
+  /** 路径(不包含origin) */
+  path: string
+  /** 路径参数 */
+  query?: BaseObj
+  /** body参数 */
+  data?: any
+  /** 请求头 */
+  headers?: BaseObj
 }
 
 /** 
  * 请求方法(默认)
+ * 带请求结果处理过程
  */
-function request(options: {
-  method: 'get' | 'post' | 'delete' | 'put'
-  url: string
-}) {
-  return fetch(options.url, {
-    method: options.method,
-  })
+async function _fetch_<T>(options: RequestType) {
+  const res = await fetch(options.path, options);
+  //TODO
+  const result: T = await res.json();
+  return result;
 }
 
 /** 
  * 请求类封装
  */
 export class Request {
-  constructor(options?: Partial<OptionsType>) {
+  constructor(options?: Partial<RequestInitType>) {
     if (options) this.init(options);
   }
 
-  /** 请求公共url */
-  private baseUrl?: string;
+  /** 请求baseUrl */
+  private origin?: string;
 
-  /** 请求方法 */
-  private request?: typeof request;
+  /** 请求方法, 带请求结果处理过程 */
+  private request = _fetch_;
 
   /** 初始化sdk */
-  init(options: Partial<OptionsType>) {
-    this.baseUrl = options.baseUrl;
-    this.request = options.request;
+  init(options: Partial<RequestInitType>) {
+    this.origin = options.origin;
+    this.request = options.request || _fetch_;
   }
 
   /** 发送http请求 */
-  async private sendRequest<T>(options: {
-    method: 'get' | 'post' | 'delete' | 'put'
-    path: string
-    
-  }) {
-    const api = this.request || request;
-
-    console.log('发送请求', options);
-
-    const res = await api({
-      method: options.method,
-      url: `${this.baseUrl}${options.path}`
+  async protected sendRequest<T>(options: RequestType) {
+    const res = await this.request<T>({
+      ...options,
+      path: `${this.origin}${options.path}`
     });
-
-    return res as any as T
+    //TODO 处理结果
+    return res;
   }
-
-  /** 发送get请求 */
-  async protected get<T>(path: string) {
-    return this.sendRequest<T>({
-      method: 'get',
-      path,
-    })
-  }
-
-  /** 发送post请求 */
-  async protected post<T>(path: string) {
-    return this.sendRequest<T>({
-      method: 'post',
-      path,
-    })
-  }
-
 }
 
 /**
  * SDK
  */
 export class NestSDK extends Request {
-  constructor(options?: Partial<OptionsType>) {
+  constructor(options?: Partial<RequestInitType>) {
     super(options);
   }
 
@@ -91,16 +90,31 @@ export class NestSDK extends Request {
   private _xxxController = {
     /** 查询所有应用 */
     findAllApplication: () => {
-      return this.get('/applications')
+      return this.sendRequest({
+        method: 'get',
+        path: '/applications',
+        query: {
+          a: 1,
+        }
+      })
     },
     /**
      * 新增应用
-     * @param params 参数
+     * @param query 参数
      * @param validate 是否校验参数
      * @returns 响应结果
      */
-    createApplication: (params: ApplicationDtoCreate, validate=false) => {
-      return this.post<ApplicationDto>('/application')
+    createApplication: (query: ApplicationDtoCreate, data: ApplicationDtoCreate, validate=false) => {
+      return this.sendRequest<ApplicationDto>({
+        method: 'post',
+        path: '/application/{id}',
+        query: {
+          a: 1,
+        },
+        data: {
+          b: 'b',
+        }
+      })
     }
   }
   readonly xxxController: Readonly<typeof this._xxxController> = this._xxxController;
@@ -110,9 +124,9 @@ export class NestSDK extends Request {
 /** 
  * 使用示例
  */
-const client = new NestSDK({baseUrl: 'httpss'});
+const client = new NestSDK({origin: 'httpss'});
 client.init({
-  baseUrl: 'http://localhost:9000'
+  origin: 'http://localhost:9000'
 });
 
 console.log(client.xxxController);
